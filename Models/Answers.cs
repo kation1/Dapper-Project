ac287f0d11f8a78a48a9c66c97e38f32f3cf88d0
Eugene Hill <eugenehill@quickenloans.com>
2020-09-18 11:15:31 -0400

    Added style

--- Models/Answers.cs
+++ Models/Answers.cs
@@ -23,7 +23,7 @@ namespace Dapper_Project.Models
         public int UpVotes { get; set; }
 
 
-        const string server = "Server=9QP7Q13\\SQLEXPRESS;Database=Slack;user id=sa;password=abc123";
+        const string server = "Server=4797Q13;Database=Slack;user id=sa;password=Inari2007";
 
         public static Answers Read(int _id)
         {
